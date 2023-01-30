b16b3f12df7784d5659253f495d2df47d4b392e1
marcos <marcosbuydid@gmail.com>
2023-01-30 02:45:06 -0300

    install and configure react navigation drawer

--- navigationApp/App.tsx
+++ navigationApp/App.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigators/StackNavigator';
+import { SideBasicMenu } from './src/navigators/SideBasicMenu';
 
 const App = () => {
   return (
     
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <SideBasicMenu />
     </NavigationContainer>
   )
 }
---
+++ navigationApp/src/navigators/SideBasicMenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StackNavigator } from './StackNavigator';
+import { SettingsScreen } from '../screens/SettingsScreen';
+
+const Drawer = createDrawerNavigator();
+
+export const SideBasicMenu = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    </Drawer.Navigator>
+  );
+}
