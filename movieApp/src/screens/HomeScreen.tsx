58ee987bb4c726d5465c15281d278279117f41e0
marcos <marcosbuydid@gmail.com>
2023-02-10 22:18:19 -0300

    configure axios and movie interface

--- movieApp/src/screens/HomeScreen.tsx
+++ movieApp/src/screens/HomeScreen.tsx
@@ -1,8 +1,17 @@
 import 'react-native-gesture-handler';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
+import movieDB from '../api/MovieDB';
+import { MovieDBNowPlaying } from '../interfaces/MovieInterface';
 
 export const HomeScreen = () => {
+
+    useEffect(() => {
+        movieDB.get<MovieDBNowPlaying>('/now_playing')
+            .then(response => { console.log(response.data.results[0].title) });
+    }, [])
+
+
     return (
         <View>
             <Text>Home Screen</Text>
