c91bc91648b3cd86d92d1dabbf4bcbedeaba776b
marcos <marcosbuydid@gmail.com>
2023-02-15 21:44:23 -0300

    install and configure react native snap carousel

--- movieApp/src/screens/HomeScreen.tsx
+++ movieApp/src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react'
-import { ActivityIndicator, Text, View } from 'react-native'
+import { ActivityIndicator, View, Dimensions } from 'react-native'
+import Carousel from 'react-native-snap-carousel';
 import { UseMovies } from '../hooks/UseMovies'
 import { MoviePoster } from '../components/MoviePoster';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -9,6 +10,8 @@ export const HomeScreen = () => {
 
     const { nowPlayingMovies, isLoading } = UseMovies();
     const { top } = useSafeAreaInsets();
+
+    const { width: windowWidth } = Dimensions.get('window');
 
     if (isLoading) {
         return (
@@ -20,10 +23,13 @@ export const HomeScreen = () => {
 
     return (
         <View style={{ marginTop: top + 20 }}>
-            <MoviePoster
-                movie={nowPlayingMovies[0]}
+            <Carousel
+                data={nowPlayingMovies}
+                //item refers to all the movies that are playing now
+                renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                sliderWidth={windowWidth}
+                itemWidth={300}
             />
-
         </View>
     )
 }
