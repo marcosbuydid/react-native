add3cf956f199e902e10cea5b8568acebbbd1983
marcos <marcosbuydid@gmail.com>
2023-02-28 10:11:42 -0300

    add switch component

--- RNComponents/src/screens/HomeScreen.tsx
+++ RNComponents/src/screens/HomeScreen.tsx
@@ -1,38 +1,11 @@
 import React from 'react'
-import { FlatList, Text, View } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FlatList, View } from 'react-native'
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
-import { MenuItem } from '../interfaces/AppInterface';
+import { HeaderTitle } from '../components/HeaderTitle';
+import { menuItems } from '../data/MenuItems';
 import { styles } from '../theme/appTheme';
 
-const menuItems: MenuItem[] = [
-    {
-        name: 'Animation 101',
-        icon: 'cube-outline',
-        component: 'Animation101Screen'
-    },
-
-    {
-        name: 'Animation 102',
-        icon: 'albums-outline',
-        component: 'Animation102Screen'
-    },
-
-]
-
 export const HomeScreen = () => {
-
-    const { top } = useSafeAreaInsets();
-
-    const renderListHeader = () => {
-        return (
-            <View style={{ marginTop: top + 20, marginBottom: 20 }}>
-                <Text style={styles.title}>
-                    Menu Options
-                </Text>
-            </View>
-        )
-    }
 
     const itemSeparator = () => {
         return (
@@ -52,7 +25,7 @@ export const HomeScreen = () => {
                 data={menuItems}
                 renderItem={({ item }) => <FlatListMenuItem menuItem={item} />}
                 keyExtractor={(item) => item.name}
-                ListHeaderComponent={renderListHeader}
+                ListHeaderComponent={() => <HeaderTitle title={'Menu Options'} />}
                 ItemSeparatorComponent={itemSeparator}
             />
         </View>
