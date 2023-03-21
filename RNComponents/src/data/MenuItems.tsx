4cdd09ede0317cf3d7560875af32f21c1c6dce7b
marcos <marcosbuydid@gmail.com>
2023-03-21 04:18:26 -0300

    made custom infinite scroll screen

--- RNComponents/src/data/MenuItems.tsx
+++ RNComponents/src/data/MenuItems.tsx
@@ -48,4 +48,10 @@ export const menuItems: MenuItem[] = [
         icon: 'copy-outline',
         component: 'ModalScreen'
     },
+
+    {
+        name: 'Infinite Scroll',
+        icon: 'download-outline',
+        component: 'InfiniteScrollScreen'
+    },
 ]
--- RNComponents/src/navigators/StackNavigator.tsx
+++ RNComponents/src/navigators/StackNavigator.tsx
@@ -8,6 +8,7 @@ import { TextInputScreen } from '../screens/TextInputScreen';
 import { PullToRefreshScreen } from '../screens/PullToRefreshScreen';
 import { SectionListScreen } from '../screens/SectionListScreen';
 import { ModalScreen } from '../screens/ModalScreen';
+import { InfiniteScrollScreen } from '../screens/InfiniteScrollScreen';
 
 const Stack = createStackNavigator();
 
@@ -28,6 +29,7 @@ export const StackNavigator = () => {
             <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen} />
             <Stack.Screen name="SectionListScreen" component={SectionListScreen} />
             <Stack.Screen name="ModalScreen" component={ModalScreen} />
+            <Stack.Screen name="InfiniteScrollScreen" component={InfiniteScrollScreen} />
         </Stack.Navigator>
     );
 }
